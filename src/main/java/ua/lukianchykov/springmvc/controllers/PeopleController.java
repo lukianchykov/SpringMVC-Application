9637b38f3e8ec104423111dddf4adbe4e350a649
Andrey <lukigor61@gmail.com>
2021-08-05 10:43:58 +0300

    PersonDAO POST method implemented

--- src/main/java/ua/lukianchykov/springmvc/controllers/PeopleController.java
+++ src/main/java/ua/lukianchykov/springmvc/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ua.lukianchykov.springmvc.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ua.lukianchykov.springmvc.dao.PersonDAO;
+import ua.lukianchykov.springmvc.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -32,6 +31,18 @@ public class PeopleController {
                        Model model){
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person){
+        return "people/new";
+    }
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        /**
+            Redirect - как goto только для браузера
+         */
+        return "redirect:/people";
     }
 
 }
